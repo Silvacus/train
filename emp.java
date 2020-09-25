3165480061949d4a3e27257e9ba82460b7af128e
jeetkumaryadav <jeetkishoreyadav@gmail.com>
2020-09-25 18:14:45 +0530

    UC1

--- emp.java
+++ emp.java
@@ -1,4 +1,7 @@
 public class emp{
 public static void main(String[] args){
 System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
+int attend =(int)Math.floor(Math.random()*10)%2;
+if(attend==0)System.out.println("Emp is present");
+else System.out.println("Emp is absent");
 }}
