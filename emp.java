fc6b95ab9447774da9951f0dc4c5465b96fe831d
jeetkumaryadav <jeetkishoreyadav@gmail.com>
2020-09-25 18:42:38 +0530

    UC5 done

--- emp.java
+++ emp.java
@@ -3,6 +3,8 @@ public static void main(String[] args){
 System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
 int hrs=0;
 int wage=0;
+int monWage=0;
+for(int day =0; day < 20; day++){
 int attend =(int)Math.floor(Math.random()*10)%3;
 switch(attend){
 case 1: hrs=8;break;
@@ -10,4 +12,5 @@ case 2: hrs=4;break;
 default: hrs=0;
 }
 wage=hrs*20;
-System.out.println("Emp Wage: "+wage);}}
+monWage=monWage+wage;}
+System.out.println("Monthly Emp Wage: "+monWage);}}
