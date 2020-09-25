7907374cad1289bf797aa9f834b18c5aa030cebb
jeetkumaryadav <jeetkishoreyadav@gmail.com>
2020-09-25 18:28:58 +0530

    UC3 done

--- emp.java
+++ emp.java
@@ -1,7 +1,12 @@
 public class emp{
 public static void main(String[] args){
 System.out.println("Welcome to Employee Wage Computation Program on Master Branch");
-int attend =(int)Math.floor(Math.random()*10)%2;
-if(attend==0)System.out.println("Emp is present");
+int hrs=0;
+int wage=0;
+int attend =(int)Math.floor(Math.random()*10)%3;
+if(attend==1){ hrs=8;}
+else if(attend==2)hrs=4;
+
 else System.out.println("Emp is absent");
-}}
+wage=hrs*20;
+System.out.println("Emp Wage: "+wage);}}
